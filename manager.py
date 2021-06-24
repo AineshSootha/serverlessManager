58f9823c7f5c7fe6ccdab247930f93cf3903ce94
Ainesh Sootha <aineshsootha@gmail.com>
2021-06-24 14:30:14 -0400

    made more indent fixes, added support for single deployment

--- manager.py
+++ manager.py
@@ -8,7 +8,7 @@ def createSls(slsPath):
     service = input('Service Name: ')
     region = input('Region: ')
     stage = input('Stage: ')
-    sls = f'service: {service}\n\nframeworkVersion: \'2\'\n\nprovider:\n  name: aws\n  runtime: nodejs12.x\n  lambdaHashingVersion: 20201221\n  stage: {stage}\n  region: {region}\npackage:\n  individually: true\n\nfunctions:\n'
+    sls = f'service: {service}\n\nframeworkVersion: \'2\'\n\nprovider:\n  name: aws\n  runtime: nodejs12.x\n  lambdaHashingVersion: 20201221\n  stage: {stage}\n    region: {region}\npackage\n  individually: true\n\nfunctions:'
     with open('serverless.yml', 'w') as fSls:
         fSls.write(sls)
         
@@ -22,15 +22,19 @@ def addTosls(fname, module, funName):
                 break
             j += 1
         j += 1
-        dataLines.insert(j, f'  {funName}:\n    handler: {module}\n    package:\n      patterns:\n        - \'!./**\'\n        - \'{fname}\'\n    events:\n      - http:\n        path: test\n        method: get\n')
+        dataLines.insert(j, f'  {funName}:\n    handler: {module}\n    package:\n      patterns:\n        - \'!./**\'\n        - \'{fname}\'\n    events:\n      - http:\n          path: test\n        method: get\n')
         
     with open('serverless.yml', 'w') as fSls:    
         dataFinal = "".join(dataLines) 
         fSls.write(dataFinal)
 
   
-def addToBSpec(env_name):
-    fBspecLines = f'version: 0.1\nphases:\n  install:\n    commands:\n      - echo install commands\n      - npm install -g serverless\n  pre_build:\n    commands:\n      - echo No pre build commands yet\n  build:\n    commands:\n      - echo Build Deploy\n      - sls deploy -v -s $ENV_NAME_{env_name}\n  post_build:\n    commands:\n      - echo post build completed on `date`'
+def addToBSpec(env_name, allorOne, funName):
+    deployCmd = f'sls deploy -v -s $ENV_NAME_{env_name} -f {funName}'
+    if allorOne == 'A' or allorOne == 'a':
+        deployCmd = f'sls deploy -v -s $ENV_NAME_{env_name}'
+
+    fBspecLines = f'version: 0.1\nphases:\n  install:\n    commands:\n      - echo install commands\n      - npm install -g serverless\n  pre_build:\n    commands:\n      - echo No pre build commands yet\n  build:\n    commands:\n      - echo Build Deploy\n      - {deployCmd}\n  post_build:\n    commands:\n      - echo post build completed on `date`'
     with open('buildspec.yml', 'w') as fBspec:
         fBspec.writelines(fBspecLines)
         
@@ -44,7 +48,7 @@ def main(skip, buildspec):
     if not slsPath.exists():
         createSls(slsPath)
 
-    env_name = input(f"\nENV_NAME: ")
+    env_name = input(f"ENV_NAME: ")
     module = input("Name of Module (Eg: handler.firstFun): ")
     funName = input("Name of Function: ")
     fName = module.split('.')[0] + '.js'
@@ -68,9 +72,10 @@ def main(skip, buildspec):
     
     addTosls(fName, module, funName)
     print(f"If you want to add more properties, \n{Fore.YELLOW}visit: https://www.serverless.com/framework/docs/providers/aws/guide/serverless.yml/{Style.RESET_ALL}\n")
-    print(f"{Fore.YELLOW}If you would like to add more files (Other than \'{fName}\') to the lambda function, edit serverless.yml and add the required files to the \'package\' section under your newly added function.{Style.RESET_ALL}\n")
+    print(f"{Fore.YELLOW}If you would like to add more files (Other than '{fName}'') to the lambda function, edit serverless.yml and add the required files to the \'package\' section under your newly added function.{Style.RESET_ALL}\n")
     print(f"{Fore.GREEN}Adding {env_name} to buildspec.yml.{Style.RESET_ALL} \nCheck Environment Variable config here: LINK")
-    addToBSpec(env_name)
+    allorOne = input(f"Press A if you would like to deploy all lambda functions in repo (N otherwise): ")
+    addToBSpec(env_name, allorOne, funName)
     
 
 
