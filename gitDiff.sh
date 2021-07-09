aadea6dc9ea8519b2385a0304ea95e8cdf5b2a42
Ainesh Sootha <aineshsootha@gmail.com>
2021-07-09 13:25:04 -0400

    Adding support for deleting aliases

--- gitDiff.sh
+++ gitDiff.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 readarray -t  arr2 < <(git diff currLatest Latest --name-only)
+delVar=0
 for i in "${arr2[@]}"; do
     if [[ "${i}" == *"/"* ]]; then
-        funName=$(dirname "$i")
-        echo -e "serverless deploy function --function ${funName}\n"  >> deploy.sh
+        if [ ! -f "$i" ]; then
+            delVar=1
+            break
+        fi
     fi
 done
+if [ "$delVar" == 1 ]; then
+    echo -e "serverless deploy" >> deploy.sh
+    exit 0
+else
+    for i in "${arr2[@]}"; do
+        if [[ "${i}" == *"/"* ]]; then
+            funName=$(dirname "$i")
+            echo -e "serverless deploy function --function ${funName}\n"  >> deploy.sh
+        fi
+    done
+fi
 
 if [ -s "deploy.sh" ]; then
-    echo "full"
+    echo "deploy.sh created"
 else
-    echo "EMPTY"
     echo -e "serverless deploy" >> deploy.sh
 fi
